cdab469f3c483a35abcf94850d1754e1a1a02948
Billy4195 <g4691821@gmail.com>
2015-11-12 22:10:52 +0800

    infix_to_postfix
    input until eof

--- infix_to_postfix/in_to_post.cpp
+++ infix_to_postfix/in_to_post.cpp
@@ -11,30 +11,33 @@ int main(){
 
     cin.get(bit);
     //cout << bit <<"Q"<<endl;
-    while(bit != '\n'){
-        if( (bit >= 'A' && bit <= 'z') || (bit>='0' && bit <='9') ){
-            cout <<bit;
-            if(!stk.empty() && (stk.top() == '*' || stk.top() == '/') ){
-                cout << stk.top();
-                stk.pop();
-            }
-        }else if(bit == '*' || bit == '/'){
+    while(!cin.eof()){
+        while(bit != '\n'){
+            if( (bit >= 'A' && bit <= 'z') || (bit>='0' && bit <='9') ){
+                cout <<bit;
+                if(!stk.empty() && (stk.top() == '*' || stk.top() == '/') ){
+                    cout << stk.top();
+                    stk.pop();
+                }
+            }else if(bit == '*' || bit == '/'){
 
-            stk.push(bit);
-        }else if(bit == '+' || bit == '-'){
-            if(!stk.empty() && (stk.top() == '+' || stk.top() == '-') ){
-                cout << stk.top();
-                stk.pop();
+                stk.push(bit);
+            }else if(bit == '+' || bit == '-'){
+                if(!stk.empty() && (stk.top() == '+' || stk.top() == '-') ){
+                    cout << stk.top();
+                    stk.pop();
+                }
+                stk.push(bit);
             }
-            stk.push(bit);
+            cin.get(bit);
+    //        cout << bit <<"OWO" <<endl;
         }
-        cin.get(bit);
-//        cout << bit <<"OWO" <<endl;
+        while(stk.size()){
+            cout << stk.top();
+            stk.pop();
+        }
+        cout << endl;
+    cin.get(bit);
     }
-    while(stk.size()){
-        cout << stk.top();
-        stk.pop();
-    }
-
     return 0;
 }
